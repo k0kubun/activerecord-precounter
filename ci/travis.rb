f2e5a5374d944db6fd6b8b287d9417180986eeab
Takashi Kokubun <takashikkbn@gmail.com>
2017-08-12 03:11:15 -0700

    Run tests on TravisCI

---
+++ ci/travis.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+commands = [
+  'mysql -e "create database activerecord_unittest;"',
+  'mysql -e "create database activerecord_unittest2;"',
+  'psql  -c "create database activerecord_unittest;" -U postgres',
+  'psql  -c "create database activerecord_unittest2;" -U postgres'
+]
+
+commands.each do |command|
+  system("#{command} > /dev/null 2>&1")
+end
+
+exit system("bundle exec rake $TASK")
